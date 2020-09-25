b9b89fbc9418f1d877e858207990bf0c15d0d017
j-gens <jgens2@gmail.com>
2020-09-25 13:49:05 -0700

    Add links to all pages to navigation component

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 export default function Nav() {
   return (
     <nav>
       <ul>
-        <li>Home</li>
-        <li>Beers</li>
+        <li>
+          <Link to='/'>Hot Now</Link>
+        </li>
+        <li>
+          <Link to='/pizzas'>Pizzas</Link>
+        </li>
+        <li>
+          <Link to='/'>LOGO</Link>
+        </li>
+        <li>
+          <Link to='/masters'>Masters</Link>
+        </li>
+        <li>
+          <Link to='/order'>Order Ahead</Link>
+        </li>
       </ul>
     </nav>
   );
