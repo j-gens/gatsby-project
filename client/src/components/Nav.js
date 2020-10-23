d8ea7562d5ec0730bb11c3da0643c1bba09f6740
j-gens <jgens2@gmail.com>
2020-10-23 13:54:03 -0700

    Import Logo component and place in center of navigation bar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Link } from 'gatsby';
+import Logo from './Logo';
+
+
+const NavStyles = styled.nav`
+  margin-bottom: 3rem;
+  ul {
+    margin: 0;
+    padding: 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr auto 1fr 1fr;
+    grid-gap: 2rem;
+    align-items: center;
+    list-style: none;
+    text-align: center;
+  }
+  li {
+    --rotate: -2deg;
+    transform: rotate(var(--rotate));
+    order: 1;
+    &:nth-child(1) {
+      --rotate: 1deg;
+    }
+    &nth-child(2) {
+      --rotate: -3deg;
+    }
+    &nth-child(4) {
+      --rotate: 2.5deg;
+    }
+    &:hover {
+      --rotate: 3deg;
+    }
+  }
+  a {
+    font-size: 3rem;
+    text-decoration: none;
+    &:hover {
+      color: var(--red);
+    }
+  }
+`;
 
 export default function Nav() {
   return (
-    <nav>
+    <NavStyles>
       <ul>
         <li>
           <Link to='/'>Hot Now</Link>
@@ -12,7 +53,7 @@ export default function Nav() {
           <Link to='/pizzas'>Pizzas</Link>
         </li>
         <li>
-          <Link to='/'>LOGO</Link>
+          <Link to='/'><Logo /></Link>
         </li>
         <li>
           <Link to='/masters'>Masters</Link>
@@ -21,6 +62,6 @@ export default function Nav() {
           <Link to='/order'>Order Ahead</Link>
         </li>
       </ul>
-    </nav>
+    </NavStyles>
   );
 }
